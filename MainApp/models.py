929b48f8c5171cef05d4bc92d807b155692b3f0e
Hallie Coronado <hallie_coronado1@baylor.edu>
2022-05-04 15:35:42 -0500

    cool

---
+++ MainApp/models.py
@@ -0,0 +1,18 @@
+from enum import auto
+from django.db import models
+
+# Create your models here.
+
+class Pizza(models.Model):
+    pizza_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.pizza_name
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    topping_name = models.CharField(max_length=200)
+    class Meta:
+            verbose_name_plural = 'toppings'
+    def __str__(self):
+        return f"{self.topping_name[:50]}..."
